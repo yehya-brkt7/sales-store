5e4754e3a2e4cf9c8abceacc85d5f44eaf5c187f
Yehya brkt <yaya.bt747@gmail.com>
2023-06-02 03:00:34 +0300

    added swiperjs

--- Frontend/app/components/products/trendingproducts.js
+++ Frontend/app/components/products/trendingproducts.js
@@ -8,14 +8,14 @@ import Badge from "react-bootstrap/Badge";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { useRouter } from "next/navigation";
-import Carousel from "react-elastic-carousel";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
 
-const breakPoints = [
-  { width: 1, itemsToShow: 1 },
-  { width: 550, itemsToShow: 1 },
-  { width: 768, itemsToShow: 2 },
-  { width: 1200, itemsToShow: 3 },
-];
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
 const Trending = ({ products }) => {
   const { productcolor, setproductcolor, setrelatedproducttype } = useStore(
@@ -46,145 +46,170 @@ const Trending = ({ products }) => {
       <h1>Trending</h1>
 
       <div className={styles.container}>
-        <Carousel breakPoints={breakPoints} disableArrowsOnEnd={false}>
+        <Swiper
+          // install Swiper modules
+          modules={[Navigation, Pagination, Scrollbar, A11y]}
+          spaceBetween={10}
+          navigation
+          pagination={{ clickable: true }}
+          breakpoints={{
+            400: {
+              slidesPerView: 1.2,
+            },
+            740: {
+              slidesPerView: 1.5,
+            },
+            890: {
+              slidesPerView: 2.2,
+            },
+            1324: {
+              slidesPerView: 3,
+            },
+          }}
+          className={styles.slider}
+        >
           {products
 
             .filter((product) => product.attributes[2].options[0] == "true")
             .sort((a, b) => b.price - a.price)
             .map((p, index) => {
               return (
-                <div
-                  onMouseEnter={() => {
-                    setproductId(p.id);
-                  }}
-                  data-aos="zoom-in-up"
-                  key={index}
-                  product={p}
-                  index={index}
-                  className={styles.productcontainer}
-                >
-                  <div className={styles["page-wrapper"]}>
-                    <div className={styles["page-inner"]}>
-                      <div className={styles.row}>
-                        <div className={styles["el-wrapper"]}>
-                          <div className={styles["box-up"]}>
-                            <Badge className={styles.badge} bg="danger">
-                              Trending
-                            </Badge>
-                            {p.id === productIndex ? (
-                              <Image
-                                loading="lazy"
-                                width="200"
-                                height="200"
-                                className={styles.img}
-                                src={
-                                  p.images.find((i) =>
-                                    i.name
-                                      .toLowerCase()
-                                      .includes(productcolor.toLowerCase())
-                                  ).src
-                                }
-                                alt=""
-                              />
-                            ) : (
-                              <Image
-                                loading="lazy"
-                                width="200"
-                                height="200"
-                                className={styles.img}
-                                src={p.images[0].src}
-                                alt=""
-                              />
-                            )}
-                            <div className={styles["img-info"]}>
-                              <div className={styles["info-inner"]}>
-                                <span className={styles["p-name"]}>
-                                  {p.name}
-                                </span>
+                <SwiperSlide>
+                  <div
+                    onMouseEnter={() => {
+                      setproductId(p.id);
+                    }}
+                    data-aos="zoom-in-up"
+                    key={index}
+                    product={p}
+                    index={index}
+                    className={styles.productcontainer}
+                  >
+                    <div className={styles["page-wrapper"]}>
+                      <div className={styles["page-inner"]}>
+                        <div className={styles.row}>
+                          <div className={styles["el-wrapper"]}>
+                            <div className={styles["box-up"]}>
+                              <Badge className={styles.badge} bg="danger">
+                                Trending
+                              </Badge>
+                              {p.id === productIndex ? (
+                                <Image
+                                  loading="lazy"
+                                  width="200"
+                                  height="200"
+                                  className={styles.img}
+                                  src={
+                                    p.images.find((i) =>
+                                      i.name
+                                        .toLowerCase()
+                                        .includes(productcolor.toLowerCase())
+                                    ).src
+                                  }
+                                  alt=""
+                                />
+                              ) : (
+                                <Image
+                                  loading="lazy"
+                                  width="200"
+                                  height="200"
+                                  className={styles.img}
+                                  src={p.images[0].src}
+                                  alt=""
+                                />
+                              )}
+                              <div className={styles["img-info"]}>
+                                <div className={styles["info-inner"]}>
+                                  <span className={styles["p-name"]}>
+                                    {p.name}
+                                  </span>
 
-                                {/* <span className={styles["p-company"]}>
+                                  {/* <span className={styles["p-company"]}>
                             {p.type}
                           </span> */}
-                              </div>
-                              <div className={styles["a-size"]}>
-                                Available sizes :{" "}
-                                <span className={styles.size}>
-                                  {p.attributes[0].options
-                                    .filter((s) => s != "all")
-                                    .map((s) => {
-                                      return (
-                                        <span
-                                          style={{
-                                            display: "inline-block",
-                                            color: "black",
-                                            width: "40px",
-                                            height: "20px",
-                                            marginRight: "5px",
-                                            borderRadius: "30%",
-                                          }}
-                                        >
-                                          {s}
-                                        </span>
-                                      );
-                                    })}
-                                </span>
-                              </div>
-                              <div className={styles["a-color"]}>
-                                Available color : {"  "}
-                                <span className={styles.color}>
-                                  {p.attributes[1].options
-                                    .filter((c) => c != "all")
-                                    .map((c) => {
-                                      return (
-                                        <span
-                                          className={styles.colors}
-                                          style={{
-                                            display: "inline-block",
-                                            backgroundColor: c,
-                                            width: "20px",
-                                            height: "20px",
-                                            marginRight: "5px",
-                                            borderRadius: "50%",
-                                            cursor: "pointer",
-                                          }}
-                                          key={c}
-                                          onClick={() => handleChange(c, p.id)}
-                                          tabIndex="-1"
-                                        ></span>
-                                      );
-                                    })}
-                                </span>
+                                </div>
+                                <div className={styles["a-size"]}>
+                                  Available sizes :{" "}
+                                  <span className={styles.size}>
+                                    {p.attributes[0].options
+                                      .filter((s) => s != "all")
+                                      .map((s) => {
+                                        return (
+                                          <span
+                                            style={{
+                                              display: "inline-block",
+                                              color: "black",
+                                              width: "40px",
+                                              height: "20px",
+                                              marginRight: "5px",
+                                              borderRadius: "30%",
+                                            }}
+                                          >
+                                            {s}
+                                          </span>
+                                        );
+                                      })}
+                                  </span>
+                                </div>
+                                <div className={styles["a-color"]}>
+                                  Available color : {"  "}
+                                  <span className={styles.color}>
+                                    {p.attributes[1].options
+                                      .filter((c) => c != "all")
+                                      .map((c) => {
+                                        return (
+                                          <span
+                                            className={styles.colors}
+                                            style={{
+                                              display: "inline-block",
+                                              backgroundColor: c,
+                                              width: "20px",
+                                              height: "20px",
+                                              marginRight: "5px",
+                                              borderRadius: "50%",
+                                              cursor: "pointer",
+                                            }}
+                                            key={c}
+                                            onClick={() =>
+                                              handleChange(c, p.id)
+                                            }
+                                            tabIndex="-1"
+                                          ></span>
+                                        );
+                                      })}
+                                  </span>
+                                </div>
                               </div>
                             </div>
-                          </div>
 
-                          <div className={styles["box-down"]}>
-                            <div className={styles["h-bg"]}>
-                              <div className={styles["h-bg-inner"]}></div>
+                            <div className={styles["box-down"]}>
+                              <div className={styles["h-bg"]}>
+                                <div className={styles["h-bg-inner"]}></div>
+                              </div>
+
+                              <a
+                                onClick={() =>
+                                  handleClick(p.id, p.categories[0].name)
+                                }
+                                className={styles.cart}
+                                href="#"
+                              >
+                                <span className={styles.price}>{p.price}$</span>
+
+                                <span className={styles["add-to-cart"]}>
+                                  <span className={styles.txt}>View Item</span>
+                                </span>
+                              </a>
                             </div>
-
-                            <a
-                              onClick={() =>
-                                handleClick(p.id, p.categories[0].name)
-                              }
-                              className={styles.cart}
-                              href="#"
-                            >
-                              <span className={styles.price}>{p.price}$</span>
-
-                              <span className={styles["add-to-cart"]}>
-                                <span className={styles.txt}>View Item</span>
-                              </span>
-                            </a>
                           </div>
                         </div>
                       </div>
                     </div>
                   </div>
-                </div>
+                </SwiperSlide>
               );
             })}
-        </Carousel>
+        </Swiper>
       </div>
     </main>
   );
