89ea6abf941297d303562d3bd4d739c08dff5f14
Yehya brkt <yaya.bt747@gmail.com>
2023-06-04 03:59:49 +0300

    relatedproducts slider update

--- Frontend/app/components/productdetails/relatedproducts/relatedproducts.js
+++ Frontend/app/components/productdetails/relatedproducts/relatedproducts.js
@@ -26,21 +26,20 @@ const RelatedProducts = ({ id }) => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
-  const [slidesPerView, setSlidesPerView] = useState(1);
-
-  useEffect(() => {
-    const totalProducts = storeproducts.filter(
+  const [productsLength, setProductslength] = useState(
+    storeproducts.filter(
       (product) =>
         product.categories[0].name == relatedproducttype && product.id != id
-    ).length;
+    ).length
+  );
 
-    if (totalProducts === 1) {
-      setSlidesPerView(1); // Display only 1 slide when there's only 1 product
-    } else if (totalProducts === 2) {
-      setSlidesPerView(2); // Display 2 slides when there are 2 products
-    } else {
-      setSlidesPerView(3); // Display 3 slides for all other cases
-    }
+  useEffect(() => {
+    setProductslength(
+      storeproducts.filter(
+        (product) =>
+          product.categories[0].name == relatedproducttype && product.id != id
+      ).length
+    );
   }, [storeproducts]);
 
   return (
@@ -54,92 +53,103 @@ const RelatedProducts = ({ id }) => {
           spaceBetween={20}
           navigation
           pagination={{ clickable: true }}
-          slidesPerView={slidesPerView}
+          breakpoints={{
+            300: {
+              slidesPerView: 1,
+            },
+
+            840: {
+              slidesPerView: productsLength == 1 ? 1 : 2,
+            },
+
+            1300: {
+              slidesPerView:
+                productsLength == 1 ? 1 : productsLength == 2 ? 2 : 3,
+            },
+          }}
           className={styles.slider}
         >
-          {storeproducts.length === 0 ? (
-            <h2 style={{ textAlign: "center" }}>No products found</h2>
-          ) : (
-            storeproducts
+          {storeproducts.length === 0
+            ? ""
+            : storeproducts
 
-              .filter(
-                (product) =>
-                  product.categories[0].name == relatedproducttype &&
-                  product.id != id
-              )
-              .sort((a, b) => b.price - a.price)
-              .map((p, index) => {
-                return (
-                  <SwiperSlide>
-                    <div
-                      data-aos="zoom-in-up"
-                      key={index}
-                      product={p}
-                      index={index}
-                      className={styles.productcontainer}
-                    >
-                      <div className={styles["page-wrapper"]}>
-                        <div className={styles["page-inner"]}>
-                          <div className={styles.row}>
-                            <div className={styles["el-wrapper"]}>
-                              <div className={styles["box-up"]}>
-                                <Image
-                                  loading="lazy"
-                                  width="200"
-                                  height="200"
-                                  className={styles.img}
-                                  src={
-                                    p.images.find((i) =>
-                                      i.name
-                                        .toLowerCase()
-                                        .includes("blue" || "red" || "green")
-                                    ).src
-                                  }
-                                  alt=""
-                                />
+                .filter(
+                  (product) =>
+                    product.categories[0].name == relatedproducttype &&
+                    product.id != id
+                )
+                .sort((a, b) => b.price - a.price)
+                .map((p, index) => {
+                  return (
+                    <SwiperSlide>
+                      <div
+                        data-aos="zoom-in-up"
+                        key={index}
+                        product={p}
+                        index={index}
+                        className={styles.productcontainer}
+                      >
+                        <div className={styles["page-wrapper"]}>
+                          <div className={styles["page-inner"]}>
+                            <div className={styles.row}>
+                              <div className={styles["el-wrapper"]}>
+                                <div className={styles["box-up"]}>
+                                  <Image
+                                    loading="lazy"
+                                    width="200"
+                                    height="200"
+                                    className={styles.img}
+                                    src={
+                                      p.images.find((i) =>
+                                        i.name
+                                          .toLowerCase()
+                                          .includes("blue" || "red" || "green")
+                                      ).src
+                                    }
+                                    alt=""
+                                  />
 
-                                <div className={styles["img-info"]}>
-                                  <div className={styles["info-inner"]}>
-                                    <span className={styles["p-name"]}>
-                                      {p.name}
-                                    </span>
+                                  <div className={styles["img-info"]}>
+                                    <div className={styles["info-inner"]}>
+                                      <span className={styles["p-name"]}>
+                                        {p.name}
+                                      </span>
 
-                                    {/* <span className={styles["p-company"]}>
+                                      {/* <span className={styles["p-company"]}>
                             {p.type}
                           </span> */}
+                                    </div>
                                   </div>
                                 </div>
-                              </div>
 
-                              <div className={styles["box-down"]}>
-                                <div className={styles["h-bg"]}>
-                                  <div className={styles["h-bg-inner"]}></div>
-                                </div>
+                                <div className={styles["box-down"]}>
+                                  <div className={styles["h-bg"]}>
+                                    <div className={styles["h-bg-inner"]}></div>
+                                  </div>
 
-                                <Link
-                                  href={`/products/${p.id}`}
-                                  className={styles.cart}
-                                  onClick={handleClick}
-                                >
-                                  <span className={styles.price}>
-                                    {p.price}$
-                                  </span>
-                                  <span className={styles["add-to-cart"]}>
-                                    <span className={styles.txt}>
-                                      View Item
+                                  <Link
+                                    href={`/products/${p.id}`}
+                                    className={styles.cart}
+                                    onClick={handleClick}
+                                  >
+                                    <span className={styles.price}>
+                                      {p.price}$
                                     </span>
-                                  </span>
-                                </Link>
+                                    <span className={styles["add-to-cart"]}>
+                                      <span className={styles.txt}>
+                                        View Item
+                                      </span>
+                                    </span>
+                                  </Link>
+                                </div>
                               </div>
                             </div>
                           </div>
                         </div>
                       </div>
-                    </div>
-                  </SwiperSlide>
-                );
-              })
-          )}
+                    </SwiperSlide>
+                  );
+                })}
         </Swiper>
       </div>
     </main>
